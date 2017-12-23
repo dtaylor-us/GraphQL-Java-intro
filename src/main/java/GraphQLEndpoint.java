75a8e1c0c260a2d75fab1d74a5aba777371ca0e5
Derek <derektaylor.us@gmail.com>
2017-12-22 23:26:01 -0600

    HOWTOGRAPHQL-JAVA: MUTATIONS + CONNECTORS

--- src/main/java/GraphQLEndpoint.java
+++ src/main/java/GraphQLEndpoint.java
@@ -1,4 +1,6 @@
 import com.coxautodev.graphql.tools.SchemaParser;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import graphql.schema.GraphQLSchema;
 import graphql.servlet.SimpleGraphQLServlet;
 
@@ -7,15 +9,23 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
+    private static final LinkRepository linkRepository;
+
+    static {
+        //Change to `new MongoClient("mongodb://<host>:<port>/hackernews")`
+        //if you don't have Mongo running locally on port 27017
+        MongoDatabase mongo = new MongoClient().getDatabase("hackernews");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+    }
+
     public GraphQLEndpoint() {
         super(buildSchema());
     }
 
     private static GraphQLSchema buildSchema() {
-        LinkRepository linkRepository = new LinkRepository();
         return SchemaParser.newParser()
                 .file("schema.graphqls")
-                .resolvers(new Query(linkRepository))
+                .resolvers(new Query(linkRepository), new Mutation(linkRepository))
                 .build()
                 .makeExecutableSchema();
     }
--- src/main/java/Link.java
+++ src/main/java/Link.java
@@ -1,11 +1,21 @@
 public class Link {
 
+    private final String id; //the new field
     private final String url;
     private final String description;
 
     public Link(String url, String description) {
+        this(null, url, description);
+    }
+
+    public Link(String id, String url, String description) {
+        this.id = id;
         this.url = url;
         this.description = description;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getUrl() {
@@ -15,4 +25,5 @@ public class Link {
     public String getDescription() {
         return description;
     }
+
 }
